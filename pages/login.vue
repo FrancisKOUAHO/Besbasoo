859968ba73002e1c74edf65b649305bead1af21c
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-08-16 22:43:30 +0200

    Besbasoo

--- pages/login.vue
+++ pages/login.vue
@@ -75,7 +75,7 @@
       </span>
 
             <div class="flex justify-center">
-              <img class="mt-11 desktop-view" src="../static/logo-1.png" width="65px" style="object-fit: cover;" alt="logo">
+              <img class="mt-11 desktop-view" src="logo-1.png" width="65px" style="object-fit: cover;" alt="logo">
               <img class="mt-11 mobile-logo mb-10" src="~/assets/images/logo-2.png" width="65px" style="object-fit: cover;" alt="logo">
             </div>
            <!-- desktop footer -->
