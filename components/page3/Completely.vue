006d0aeac96189ce794fe2249a0d56d956d30698
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-08-18 21:48:35 +0200

    task/Completely task/onProduct

---
+++ components/page3/Completely.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="slider-completely">
+    <h1>Les utilisateurs ont également consulté...</h1>
+
+      <VueSlickCarousel
+        :arrows="true"
+        v-bind="settings"
+        class="vue-slick-carousel"
+      >
+        <div class="box" style="margin: 4rem;" v-for="slider in 5">
+          <h3>Sculpture Femme Africaine Watusi nue  - ANZENGRUBER KERAMIK</h3>
+          <img src="~/assets/images/image-14.png" alt="" class="img-shadow-ribbon">
+          <p>Avant : <span class="price-barre-box">79,99€</span> (-20%)</p>
+          <div class="box-price">
+            <p class="box-tilte">55,99€</p>
+            <p class="box-delivery">
+              <svg id="Capa_1" enable-background="new 0 0 512 512" height="30" viewBox="0 0 512 512" width="30" xmlns="http://www.w3.org/2000/svg"><g><path d="m407 16c-57.891 0-105 47.109-105 105s47.109 105 105 105 105-47.109 105-105-47.109-105-105-105zm45 120h-30v30h-30v-30h-30v-30h30v-30h30v30h30z"/><path d="m137 376c8.3 0 292.767 0 281.602 0l33.252-128.247c-10.422 3.702-21.396 6-32.807 7.029l-.315 1.218h-70.437l1.117-13.391c-10.366-4.933-19.834-11.239-28.546-18.532l-2.661 31.923h-62.41l-5.001-60h44.059c-6.172-9.201-10.779-19.396-14.561-30h-141.261l-20.004-90h-119.027v30h94.974l53.32 240h-11.294c-24.814 0-45 20.186-45 45s20.186 45 45 45h17.763c-1.681 4.715-2.763 9.716-2.763 15 0 24.814 20.186 45 45 45s45-20.186 45-45c0-5.284-1.082-10.285-2.763-15h65.526c-1.681 4.715-2.763 9.716-2.763 15 0 24.814 20.186 45 45 45s45-20.186 45-45c0-5.284-1.082-10.285-2.763-15h32.763v-30h-285c-8.276 0-15-6.724-15-15s6.724-15 15-15zm208.795-90h65.158l-15.555 60h-54.604zm-30.09 0-5.001 60h-47.41l-5.001-60zm-82.5 60h-54.168l-13.336-60h62.503zm-87.507-150h75.007l5.001 60h-66.671zm66.302 255c0 8.276-6.724 15-15 15s-15-6.724-15-15 6.724-15 15-15 15 6.724 15 15zm150 0c0 8.276-6.724 15-15 15s-15-6.724-15-15 6.724-15 15-15 15 6.724 15 15z"/></g></svg>
+            </p>
+          </div>
+          <div class="cross-shadow-ribbon">PROMO</div>
+        </div>
+      </VueSlickCarousel>
+    </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+  name: "Completely",
+  components: {VueSlickCarousel},
+  data() {
+    return {
+      settings: {
+        "infinite": true,
+        "initialSlide": 2,
+        "speed": 500,
+        "slidesToShow": 3,
+        "slidesToScroll": 1,
+        "swipeToSlide": true
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.slider-completely {
+  margin: 1rem;
+  width: 1403px;
+  height: 473px;
+  padding: 1rem;
+  font-family: Rambla, serif;
+
+  h1 {
+    font-size: 30px;
+    font-weight: bold;
+  }
+
+  .vue-slick-carousel {
+    width: 1300px;
+    height: 280px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 2rem;
+
+    .box-price{
+      display: flex;
+      justify-content: space-between;
+
+    }
+
+    .box-delivery{
+      color: #ff9a24;
+    }
+    .box-tilte{
+      color: #ff9a24;
+      font-weight: bold;
+      font-size: 25px;
+    }
+
+    .img-shadow-ribbon{
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    .price-barre-box {
+      font-family: Rambla, serif;
+      text-decoration: line-through;
+      font-size: 13px;
+    }
+
+    .slick-arrow {
+      width: 2rem;
+      color: #e40000 !important;
+      background: #e40000 !important;
+      margin-left: -1rem;
+      height: 3rem;
+    }
+
+    .slick-next {
+      width: 2rem;
+      color: #e40000 !important;
+      background: #e40000 !important;
+      margin-right: -1rem;
+      height: 3rem;
+    }
+
+    .box {
+      width: 80% !important;
+      padding: 1rem;
+      border: 1px solid #CCC;
+      background: #FFF;
+      height: 290px;
+      position: relative;
+      border-radius: 3px;
+    }
+
+    .cross-shadow-ribbon {
+      position: absolute;
+      background: #E40000;
+      top: 8em;
+      left: -2.9em;
+      padding: 8px 15px;
+      text-align: center;
+      color: #FFF;
+      transform: rotate(-90deg)
+    }
+
+    .cross-shadow-ribbon:before {
+      content: "";
+      position: absolute;
+      left: -20px;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      width: 0;
+      height: 0;
+      border-bottom: 20px solid #ab0202;
+      border-left: 20px solid transparent;
+    }
+
+
+  }
+
+
+}
+
+</style>
--- pages/products/oneProduct.vue
+++ pages/products/oneProduct.vue
@@ -1,34 +1,35 @@
 <template>
-  <section class="section-oneProduct">
-    <div class="picture-secondes">
-      <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
-      <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
-      <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
-      <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
-      <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
-    </div>
-
-    <div class="picture-principal">
-      <img src="~/assets/images/image-14.png" alt="">
-      <div class="cercle">
-        <button class="toggle-favorite" @click="toggle">
-          <FavoriteIcon
-            class="toggle-favorite__icon heart"
-            :class="iconClasses"
-            @animationend.native="onIconAnimationEnds"
-            width="30"
-          />
-          <transition name="favorite-particles-transition">
-            <div v-if="animating" class="toggle-favorite__particles"></div>
-          </transition>
-        </button>
+  <section>
+    <div class="section-oneProduct">
+      <div class="picture-secondes">
+        <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
+        <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
+        <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
+        <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
+        <div class="box-picture"><img src="~/assets/images/image-14.png" alt=""></div>
       </div>
-    </div>
 
-    <div class="description-principal">
-      <h1>Sculpture Femme Africaine Watusi nue - ANZENGRUBER KERAMIK</h1>
+      <div class="picture-principal">
+        <img src="~/assets/images/image-14.png" alt="">
+        <div class="cercle">
+          <button class="toggle-favorite" @click="toggle">
+            <FavoriteIcon
+              class="toggle-favorite__icon heart"
+              :class="iconClasses"
+              @animationend.native="onIconAnimationEnds"
+              width="30"
+            />
+            <transition name="favorite-particles-transition">
+              <div v-if="animating" class="toggle-favorite__particles"></div>
+            </transition>
+          </button>
+        </div>
+      </div>
 
-      <div class="group-stars">
+      <div class="description-principal">
+        <h1>Sculpture Femme Africaine Watusi nue - ANZENGRUBER KERAMIK</h1>
+
+        <div class="group-stars">
         <span v-for="star in 4">
            <svg class="mx-1 w-4 h-4 fill-current text-yellow-500" xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20">
@@ -36,58 +37,121 @@
             d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
         </svg>
         </span>
-        <svg class="mx-1 w-4 h-4 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg"
-             viewBox="0 0 20 20">
-          <path
-            d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
-        </svg>
-        <div class="avis-product">46 avis</div>
-        <div class="share">
-          <svg height="20" viewBox="-21 0 512 512" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m453.332031 85.332031c0 38.292969-31.039062 69.335938-69.332031 69.335938s-69.332031-31.042969-69.332031-69.335938c0-38.289062 31.039062-69.332031 69.332031-69.332031s69.332031 31.042969 69.332031 69.332031zm0 0"/><path d="m384 170.667969c-47.0625 0-85.332031-38.273438-85.332031-85.335938 0-47.058593 38.269531-85.332031 85.332031-85.332031s85.332031 38.273438 85.332031 85.332031c0 47.0625-38.269531 85.335938-85.332031 85.335938zm0-138.667969c-29.417969 0-53.332031 23.9375-53.332031 53.332031 0 29.398438 23.914062 53.335938 53.332031 53.335938s53.332031-23.9375 53.332031-53.335938c0-29.394531-23.914062-53.332031-53.332031-53.332031zm0 0"/><path d="m453.332031 426.667969c0 38.289062-31.039062 69.332031-69.332031 69.332031s-69.332031-31.042969-69.332031-69.332031c0-38.292969 31.039062-69.335938 69.332031-69.335938s69.332031 31.042969 69.332031 69.335938zm0 0"/><path d="m384 512c-47.0625 0-85.332031-38.273438-85.332031-85.332031 0-47.0625 38.269531-85.335938 85.332031-85.335938s85.332031 38.273438 85.332031 85.335938c0 47.058593-38.269531 85.332031-85.332031 85.332031zm0-138.667969c-29.417969 0-53.332031 23.9375-53.332031 53.335938 0 29.394531 23.914062 53.332031 53.332031 53.332031s53.332031-23.9375 53.332031-53.332031c0-29.398438-23.914062-53.335938-53.332031-53.335938zm0 0"/><path d="m154.667969 256c0 38.292969-31.042969 69.332031-69.335938 69.332031-38.289062 0-69.332031-31.039062-69.332031-69.332031s31.042969-69.332031 69.332031-69.332031c38.292969 0 69.335938 31.039062 69.335938 69.332031zm0 0"/><path d="m85.332031 341.332031c-47.058593 0-85.332031-38.269531-85.332031-85.332031s38.273438-85.332031 85.332031-85.332031c47.0625 0 85.335938 38.269531 85.335938 85.332031s-38.273438 85.332031-85.335938 85.332031zm0-138.664062c-29.417969 0-53.332031 23.933593-53.332031 53.332031s23.914062 53.332031 53.332031 53.332031c29.421875 0 53.335938-23.933593 53.335938-53.332031s-23.914063-53.332031-53.335938-53.332031zm0 0"/><path d="m135.703125 245.761719c-7.425781 0-14.636719-3.863281-18.5625-10.773438-5.824219-10.21875-2.238281-23.253906 7.980469-29.101562l197.949218-112.851563c10.21875-5.867187 23.253907-2.28125 29.101563 7.976563 5.824219 10.21875 2.238281 23.253906-7.980469 29.101562l-197.953125 112.851563c-3.328125 1.898437-6.953125 2.796875-10.535156 2.796875zm0 0"/><path d="m333.632812 421.761719c-3.585937 0-7.210937-.898438-10.539062-2.796875l-197.953125-112.851563c-10.21875-5.824219-13.800781-18.859375-7.976563-29.101562 5.800782-10.238281 18.855469-13.84375 29.097657-7.976563l197.953125 112.851563c10.21875 5.824219 13.800781 18.859375 7.976562 29.101562-3.945312 6.910157-11.15625 10.773438-18.558594 10.773438zm0 0"/></svg>
-        </div>
-      </div>
-      <p class="stock">En stock (encore 20 disponibles)</p>
-      <p class="free-delivery"><strong>Livraison gratuite</strong>en France métropolitaine et DOM</p>
-      <p class="free-delivery"><strong>Délais : </strong>3-4 jours ouvrés</p>
-      <p class="free-delivery">Estimation :<strong> jeudi 17 juin</strong></p>
-
-      <div class="select-adress">
-        <svg id="Layer_1" enable-background="new 0 0 512 512" height="14" viewBox="0 0 512 512" width="14"
-             xmlns="http://www.w3.org/2000/svg">
-          <g>
+          <svg class="mx-1 w-4 h-4 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg"
+               viewBox="0 0 20 20">
             <path
-              d="m407.579 87.677c-31.073-53.624-86.265-86.385-147.64-87.637-2.62-.054-5.257-.054-7.878 0-61.374 1.252-116.566 34.013-147.64 87.637-31.762 54.812-32.631 120.652-2.325 176.123l126.963 232.387c.057.103.114.206.173.308 5.586 9.709 15.593 15.505 26.77 15.505 11.176 0 21.183-5.797 26.768-15.505.059-.102.116-.205.173-.308l126.963-232.387c30.304-55.471 29.435-121.311-2.327-176.123zm-151.579 144.323c-39.701 0-72-32.299-72-72s32.299-72 72-72 72 32.299 72 72-32.298 72-72 72z"/>
-          </g>
-        </svg>
-        <p>Sélectionner votre adresse de livraison</p>
-      </div>
+              d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
+          </svg>
+          <div class="avis-product">46 avis</div>
+          <div class="share">
+            <svg height="20" viewBox="-21 0 512 512" width="20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="m453.332031 85.332031c0 38.292969-31.039062 69.335938-69.332031 69.335938s-69.332031-31.042969-69.332031-69.335938c0-38.289062 31.039062-69.332031 69.332031-69.332031s69.332031 31.042969 69.332031 69.332031zm0 0"/>
+              <path
+                d="m384 170.667969c-47.0625 0-85.332031-38.273438-85.332031-85.335938 0-47.058593 38.269531-85.332031 85.332031-85.332031s85.332031 38.273438 85.332031 85.332031c0 47.0625-38.269531 85.335938-85.332031 85.335938zm0-138.667969c-29.417969 0-53.332031 23.9375-53.332031 53.332031 0 29.398438 23.914062 53.335938 53.332031 53.335938s53.332031-23.9375 53.332031-53.335938c0-29.394531-23.914062-53.332031-53.332031-53.332031zm0 0"/>
+              <path
+                d="m453.332031 426.667969c0 38.289062-31.039062 69.332031-69.332031 69.332031s-69.332031-31.042969-69.332031-69.332031c0-38.292969 31.039062-69.335938 69.332031-69.335938s69.332031 31.042969 69.332031 69.335938zm0 0"/>
+              <path
+                d="m384 512c-47.0625 0-85.332031-38.273438-85.332031-85.332031 0-47.0625 38.269531-85.335938 85.332031-85.335938s85.332031 38.273438 85.332031 85.335938c0 47.058593-38.269531 85.332031-85.332031 85.332031zm0-138.667969c-29.417969 0-53.332031 23.9375-53.332031 53.335938 0 29.394531 23.914062 53.332031 53.332031 53.332031s53.332031-23.9375 53.332031-53.332031c0-29.398438-23.914062-53.335938-53.332031-53.335938zm0 0"/>
+              <path
+                d="m154.667969 256c0 38.292969-31.042969 69.332031-69.335938 69.332031-38.289062 0-69.332031-31.039062-69.332031-69.332031s31.042969-69.332031 69.332031-69.332031c38.292969 0 69.335938 31.039062 69.335938 69.332031zm0 0"/>
+              <path
+                d="m85.332031 341.332031c-47.058593 0-85.332031-38.269531-85.332031-85.332031s38.273438-85.332031 85.332031-85.332031c47.0625 0 85.335938 38.269531 85.335938 85.332031s-38.273438 85.332031-85.335938 85.332031zm0-138.664062c-29.417969 0-53.332031 23.933593-53.332031 53.332031s23.914062 53.332031 53.332031 53.332031c29.421875 0 53.335938-23.933593 53.335938-53.332031s-23.914063-53.332031-53.335938-53.332031zm0 0"/>
+              <path
+                d="m135.703125 245.761719c-7.425781 0-14.636719-3.863281-18.5625-10.773438-5.824219-10.21875-2.238281-23.253906 7.980469-29.101562l197.949218-112.851563c10.21875-5.867187 23.253907-2.28125 29.101563 7.976563 5.824219 10.21875 2.238281 23.253906-7.980469 29.101562l-197.953125 112.851563c-3.328125 1.898437-6.953125 2.796875-10.535156 2.796875zm0 0"/>
+              <path
+                d="m333.632812 421.761719c-3.585937 0-7.210937-.898438-10.539062-2.796875l-197.953125-112.851563c-10.21875-5.824219-13.800781-18.859375-7.976563-29.101562 5.800782-10.238281 18.855469-13.84375 29.097657-7.976563l197.953125 112.851563c10.21875 5.824219 13.800781 18.859375 7.976562 29.101562-3.945312 6.910157-11.15625 10.773438-18.558594 10.773438zm0 0"/>
+            </svg>
+          </div>
+        </div>
+        <p class="stock">En stock (encore 20 disponibles)</p>
+        <p class="free-delivery"><strong>Livraison gratuite</strong>en France métropolitaine et DOM</p>
+        <p class="free-delivery"><strong>Délais : </strong>3-4 jours ouvrés</p>
+        <p class="free-delivery">Estimation :<strong> jeudi 17 juin</strong></p>
 
-      <div class="Product-description">
-        <h1>Description du produit</h1>
-        <article class="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam dui tortor, quis semper justo fringilla
-          ut. Fusce maximus enim elementum mi tincidunt posuere. Sed pellentesque purus ex, porta aliquam leo
-          scelerisque nec. Etiam dignissim nisi ed pellentesque purus ex, porta aliquam leo scelerisque nec.
-        </article>
-        <center>
-          <p class="read-more">Lire plus</p>
-          <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="14" height="14" viewBox="0 0 123.959 123.958" style="enable-background:new 0 0 123.959 123.958;" xml:space="preserve">
+        <div class="select-adress">
+          <svg id="Layer_1" enable-background="new 0 0 512 512" height="14" viewBox="0 0 512 512" width="14"
+               xmlns="http://www.w3.org/2000/svg">
+            <g>
+              <path
+                d="m407.579 87.677c-31.073-53.624-86.265-86.385-147.64-87.637-2.62-.054-5.257-.054-7.878 0-61.374 1.252-116.566 34.013-147.64 87.637-31.762 54.812-32.631 120.652-2.325 176.123l126.963 232.387c.057.103.114.206.173.308 5.586 9.709 15.593 15.505 26.77 15.505 11.176 0 21.183-5.797 26.768-15.505.059-.102.116-.205.173-.308l126.963-232.387c30.304-55.471 29.435-121.311-2.327-176.123zm-151.579 144.323c-39.701 0-72-32.299-72-72s32.299-72 72-72 72 32.299 72 72-32.298 72-72 72z"/>
+            </g>
+          </svg>
+          <p>Sélectionner votre adresse de livraison</p>
+        </div>
+
+        <div class="Product-description">
+          <h1>Description du produit</h1>
+          <article class="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam dui tortor, quis semper justo fringilla
+            ut. Fusce maximus enim elementum mi tincidunt posuere. Sed pellentesque purus ex, porta aliquam leo
+            scelerisque nec. Etiam dignissim nisi ed pellentesque purus ex, porta aliquam leo scelerisque nec.
+          </article>
+          <center>
+            <p class="read-more">Lire plus</p>
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                 x="0px" y="0px" width="14" height="14" viewBox="0 0 123.959 123.958"
+                 style="enable-background:new 0 0 123.959 123.958;" xml:space="preserve">
 <g>
-	<path d="M117.979,28.017h-112c-5.3,0-8,6.4-4.2,10.2l56,56c2.3,2.3,6.1,2.3,8.401,0l56-56   C125.979,34.417,123.279,28.017,117.979,28.017z"/>
+	<path
+    d="M117.979,28.017h-112c-5.3,0-8,6.4-4.2,10.2l56,56c2.3,2.3,6.1,2.3,8.401,0l56-56   C125.979,34.417,123.279,28.017,117.979,28.017z"/>
 </g>
 </svg>
-
-        </center>
+          </center>
+        </div>
       </div>
 
+      <div class="Payment-card">
+        <div class="payement-price">
+          <p class="price-tilte">Prix originale</p>
+          <p class="price-barre">79,99€</p>
+        </div>
+
+        <div class="payement-price">
+          <p class="price-tilte">Vous économisez</p>
+          <p class="price-ok">14,09€</p>
+        </div>
+
+        <div class="payement-price">
+          <p class="price-tilte">Livraison</p>
+          <p class="price-delivery">GRATUITE</p>
+        </div>
+
+        <div class="payement-price">
+          <p class="price-tilte-total">Total (ttc)</p>
+          <p class="price-total">65,90€</p>
+        </div>
+
+        <div class="group-button-price">
+          <button
+            class="p-4 hover:bg-blue-700 text-white font-bold py-2 px-4 border rounded add-cart">
+            Ajouter au panier
+          </button>
+          <button
+            class="bg-transparent font-semibold hover:text-white py-2 px-4 buy-now rounded">
+            Acheter maintenant
+          </button>
+        </div>
+
+        <hr data-v-f537270a="" class="border-divider"
+            style="margin: 20px auto 0px; width: 95%; border: 1px solid rgb(120, 120, 120); border-radius: 5px;">
+
+        <div class="fois-four">
+          <center>
+            <p class="price-tilte">Ou payez en 4x</p>
+            <p class="price-tilte">16,48 € (+ 2,18€ de frais)</p>
+          </center>
+        </div>
+      </div>
     </div>
-
-
+    <completely/>
   </section>
 </template>
 
 <script>
+import completely from '/components/page3/Completely.vue'
 export default {
+  components: {completely},
   data() {
     return {
       favorited: false,
@@ -123,7 +187,10 @@ export default {
 
 .section-oneProduct {
   display: flex;
-  margin: 2rem;
+  flex-wrap: wrap;
+  margin: 3rem 1rem;
+  width: 1403px;
+  height: 473px;
 
   .box-picture {
     width: 58px;
@@ -262,7 +329,7 @@ export default {
       color: #777777;
     }
 
-    .share{
+    .share {
       margin-left: 11rem;
     }
 
@@ -299,7 +366,7 @@ export default {
       font-family: Rambla, serif;
     }
 
-    .read-more{
+    .read-more {
       margin-top: 0.5rem;
       font-family: Rambla, serif;
       font-size: 11px;
@@ -307,6 +374,91 @@ export default {
 
   }
 
+  .Payment-card {
+    border: 1px solid #e1e1e1;
+    width: 273px;
+    height: 425px;
+    margin-left: 4rem;
+    padding: 1rem;
+
+    .payement-price {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 0.5rem;
+
+      .price-ok {
+        color: #e61010;
+      }
+
+      .price-delivery {
+        color: #e61010;
+        font-size: 0.8rem;
+      }
+
+      .price-tilte-total {
+        font-family: Rambla, serif;
+        font-weight: bold;
+        font-size: 1.3rem;
+      }
+
+      .price-total {
+        font-family: Rambla, serif;
+        font-weight: bold;
+      }
+
+
+    }
+
+    .price-tilte {
+      font-family: Rambla, serif;
+    }
+
+    .price-barre {
+      font-family: Rambla, serif;
+      text-decoration: line-through;
+    }
+
+    .group-button-price {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-content: center;
+      margin-top: 1rem;
+
+      .add-cart {
+        padding: 0.5rem;
+        width: 233px;
+        height: 40px;
+        background: #ff9a24;
+        border: #ff9a24;
+      }
+
+      .buy-now {
+        padding: 0.5rem;
+        width: 233px;
+        height: 40px;
+        margin-top: 1rem;
+        border: 1px solid #ff9a24;
+        color: #ff9a24;
+      }
+
+      .buy-now:hover {
+        background: #ff9a24;
+        color: white;
+
+      }
+    }
+
+    .fois-four {
+      padding: 1rem;
+      margin-top: 1.5rem;
+      font-weight: bold;
+      color: #787878;
+
+    }
+  }
+
 }
 
 </style>
+
