2d08991b119d641d616e2c10509baa77e3a0519c
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-08-19 13:29:12 +0200

    task/component admin.vue

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -41,11 +41,6 @@
           <i class="fas fa-table mr-3"></i>
           Vendeurs
         </router-link>
-        <router-link to="#"
-                     class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-          <i class="fas fa-calendar mr-3"></i>
-          Paramètre
-        </router-link>
       </nav>
     </aside>
 
